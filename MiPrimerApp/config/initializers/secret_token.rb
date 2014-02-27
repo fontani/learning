7d9615dba736b487356975cd6767695b3c5bbbcd
Leonardo Fontani <leo.fontani@gmail.com>
2014-02-27 12:48:26 -0300

    first commit

---
+++ MiPrimerApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiPrimerApp::Application.config.secret_token = 'b7d3ad0124c71bfda71714f3b22b8c05a826b95181f3166fbab83a413ff05cf151ef7d107ca88bd4022036c5d5b2c2edbcf264cfbfd475e9ee423fbd1000ee2b'
